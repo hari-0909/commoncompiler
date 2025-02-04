2452d325362cb868d02e9e77df3a11b783c7fb5e
hari <haribabuji2004@gmail.com>
2025-02-04 19:04:52 +0530

    test

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import Codemirror from 'codemirror'
+import React, { useEffect } from 'react';
+import Codemirror from 'codemirror';
+import 'codemirror/mode/clike/clike'; 
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css'; 
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+
 const Editor = () => {
-  useEffect(()=>{
-    async function init(){
-      Codemirror.fromTextArea(document.getElement('realtimeEditor'),{
+  useEffect(() => {
+    async function init() {
+      Codemirror.fromTextArea(document.getElementById('realtimeEditor'), {
         mode: "text/x-c++src",
-        lineNumbers: true
+        lineNumbers: true,
+        theme: 'dracula',
+        autoCloseTags: true,
+        autoCloseBrackets:true,
+        lineNumbers:true,
       });
     }
-  })
+    init();
+  }, []);
+
   return <textarea id="realtimeEditor"></textarea>;
 };
 
-export default Editor
+export default Editor;
