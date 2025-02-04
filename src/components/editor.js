7247f98a9d6c04785b8f2edb700f68c09dd2c8f8
hari <haribabuji2004@gmail.com>
2025-02-04 18:03:35 +0530

    test

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Codemirror from 'codemirror'
+const Editor = () => {
+  useEffect(()=>{
+    async function init(){
+      Codemirror.fromTextArea(document.getElement('realtimeEditor'),{
+        mode: "text/x-c++src",
+        lineNumbers: true
+      });
+    }
+  })
+  return <textarea id="realtimeEditor"></textarea>;
+};
 
-const editor = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default editor
+export default Editor
